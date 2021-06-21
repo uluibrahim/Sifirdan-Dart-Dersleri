b526a4eb2a0c43f79f738f7dc9699e06110593fd
uluibrahim <iulu17@posta.pau.edu.tr>
2021-06-21 10:54:26 +0300

    constructor method anlatıldı ve this kavramına değinildi

---
+++ Bolum5/kurucu_constructor_methodlar.dart
@@ -0,0 +1,51 @@
+main(List<String> args) {
+  /**
+  Araba honda = Araba();
+
+  honda.marka = "Honda";
+  honda.modelYili = 2020;
+  honda.otomatikMi = true;
+  honda.bilgileriGoster();
+   */
+
+  Araba opel = Araba("Opel", 2020, false);
+}
+
+class Araba {
+  String? marka;
+  int? modelYili;
+  bool? otomatikMi;
+
+  void bilgileriGoster() {
+    print(
+        "Arabanın markası: $marka, model yılı: $modelYili, otomatik mi?: $otomatikMi");
+  }
+
+  // constructor methodu class adıyla aynı isimde olur
+  // sınıf  kullanıldığında ilk çalışacak olan methodtur
+
+  /**
+  Araba() {
+  print("Kurucu (constructor) method çalıştı");
+  }
+   */
+
+  // parametre alabilen kurucu fonksiyon
+
+/**
+  Araba(String marka, int modelYili, bool otomatikMi) {
+    // fonksiyona gelen parametreler sadece fonksiyon içinde hayat bulur
+    print("Kurucu (constructor) method çalıştı");
+  }
+ */
+
+  // this kullanımı
+
+  Araba(this.marka, this.modelYili, this.otomatikMi) {
+    // this kullanarak alınan parametreleeri class içindeki değişkenlere atamış olduk
+
+    print("Kurucu (constructor) method çalıştı");
+  }
+
+  //NOT: BİR CLASS TA 1 DEN FAZLA KURUCU METHOD KULLANILAMAZ
+}
